e509054be4fb60bca4b1d015f9c881c93ce48651
霍然 <303176152@qq.com>
2019-05-22 16:11:37 +0800

    存储数据到cookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDER } from "../constants";
+// 使用sfcookies将数据存储到浏览器的cookie中
+import { bake_cookie, read_cookie } from "sfcookies";
 
 const reminder = action => {
   const { text, dueDate } = action;
@@ -10,14 +12,27 @@ const reminder = action => {
 };
 
 // reducer接收一个state 和一个action
-const reminderReducer = (state = [], action = {}) => {
+const reminderReducer = (
+  //通过read_cookie读取浏览器存储的cookie
+  state = read_cookie("reminders") || [],
+  action = {}
+) => {
+  let reminders = null;
+
   switch (action.type) {
     case ADD_REMINDER:
-      return [...state, reminder(action)];
+      reminders = [...state, reminder(action)];
+      //以key value方式进行存储
+      bake_cookie("reminders", reminders);
+      return reminders;
     case DELETE_REMINDER:
-      return state.filter(reminder => reminder.id !== action.id);
+      reminders = state.filter(reminder => reminder.id !== action.id);
+      bake_cookie("reminders", reminders);
+      return reminders;
     case CLEAR_REMINDER:
-      return [];
+      reminders = [];
+      bake_cookie("reminders", reminders);
+      return reminders;
     default:
       return state;
   }
